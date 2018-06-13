1916c1721fdef8746803e0b37bce9bdd4d15cc30
acidvegas <acid.vegas@acid.vegas>
2018-06-13 14:34:18 -0400

    initial commit

---
+++ dickserv/modules/tripsit.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# DickServ IRC Bot
+# Developed by acidvegas in Python
+# https://git.acid.vegas/dickserv
+# tripsit.py
+
+import httplib
+
+def drug(query):
+	api = httplib.get_json('http://tripbot.tripsit.me/api/tripsit/getDrug?name=' + query.replace(' ','%20'))
+	if api['err'] != True:
+		return {'name':api['data'][0]['name'],'desc':api['data'][0]['properties']['summary']}
+	else:
+		return False
